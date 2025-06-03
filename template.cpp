1f4dec79eb3f6ec46dc80c091b5a0de4d8e5f066
Khaybula <khaibula.2003@gmail.com>
2025-06-04 02:50:44 +0300

    changed template to better style

--- template.cpp
+++ template.cpp
@@ -1,11 +1,9 @@
 #pragma GCC optimize("O3")
 
- 
 #include <bits/stdc++.h>
- 
+
 using namespace std;
- 
-#define IOS ios::sync_with_stdio(false); cin.tie(0)
+
 #define ll long long
 #define ld long double
 #define mp(a, b) make_pair(a, b)
@@ -15,13 +13,17 @@ using namespace std;
 #define pli pair<ll, int>
 #define fi first
 #define se second
- 
- 
+
 const int N = 2e5 + 10;
 const ll LINF = 1e18;
 const int INF = 1e9;
-const ld PI = 4*atanl(1);
-const int dx[4] = {1,0,-1,0}, dy[4] = {0,1,0,-1};
- 
+const ld PI = 4 * atanl(1);
+const int dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1};
+
+void fastIO() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+}
+
 int main() {
 }
