5e9a60aff22eea6ba927341616b1f2ee9572dc45
Khaybula <khaibula.2003@gmail.com>
2025-07-23 23:17:40 +0300

    added mobius precalc

---
+++ number_theory/mobius.cpp
@@ -0,0 +1,25 @@
+
+int prime[N];
+int mu[N];
+void sieve_calc() {
+    std::iota(prime, prime + N, 0);
+
+    for (int i = 2; i * i < N; ++i) {
+        if (prime[i] == i) {
+            for (int j = i * i; j < N; j += i)
+                if (prime[j] == j) prime[j] = i;
+        }
+    }
+    mu[1] = 1;
+    for (int i = 2; i < N; ++i) {
+        int cur = i;
+        cur /= prime[i];
+        if (cur % prime[i] == 0) {
+            mu[i] = 0;
+        }
+        else {
+            mu[i] = mu[cur];
+        }
+    }
+}
+
