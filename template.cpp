c2d3bdb9f8b2ae0d1fc1a85421bebba55213c348
Khaybula <khaibula.2003@gmail.com>
2025-07-24 21:49:07 +0300

    slightly changed template.cpp

--- template.cpp
+++ template.cpp
@@ -11,16 +11,15 @@ using pll = pair<ll, ll>;
 using pil = pair<int, ll>;
 using pli = pair<ll, int>;
 
-void fastIO() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-}
-
 const int N = 2e5 + 10;
 const ll LINF = 1e18;
 const int INF = 1e9;
 const ld PI = 4 * atanl(1);
 const int dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1};
 
-int main() {
+void fastIO() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
 }
+
+int main() {}
