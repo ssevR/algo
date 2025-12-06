00c003a32a8927a8648f497eb39897f1f7ebc384
Khaybula <khaibula.2003@gmail.com>
2025-12-06 22:13:46 +0300

    small fixes for sparse table code

--- seg_tree/sparse_table.cpp
+++ seg_tree/sparse_table.cpp
@@ -1,14 +1,14 @@
-template <typename T, T (*f)(T, T)> struct sparse_tree {
+template <typename T, T (*f)(T, T), T(*e)()> struct sparse_table {
     int n;
     int k;
     vector<vector<T> > st;
     int log2_floor(unsigned long long i) {
         return i ? __builtin_clzll(1) - __builtin_clzll(i) : -1;
     }
-    sparse_tree(vector<T> a) {
+    sparse_tree(const vector<T>& a) {
         n = a.size();
         k = log2_floor(n) + 1;
-        st = vector(k, vector<T>(n));
+        st = vector(k, vector<T>(n, e()));
         copy(a.begin(), a.end(), st[0].begin());
 
         for (int i = 1; i < k; i++)
@@ -17,12 +17,10 @@ template <typename T, T (*f)(T, T)> struct sparse_tree {
     }
 
 
-    T op(int l, int r) {
-        if (l < 0 || r > n || l > r) throw out_of_range("given segment is outrange or incorrect");
-        if (l == r) return T();
+    T prod(int l, int r) {
+        if (l < 0 || r > n || l > r) throw out_of_range("out of range");
+        if (l == r) return e();
         int i = log2_floor(r - l);
         return f(st[i][l], st[i][r - (1 << i)]);
     }
 };
-
-
