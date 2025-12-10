b0b527e7eaf1aba88f9688260d712dbca24ff378
Khaybula <khaibula.2003@gmail.com>
2025-12-10 19:36:51 +0300

    fixed typo in lca

--- seg_tree/sparse_table.cpp
+++ seg_tree/sparse_table.cpp
@@ -5,7 +5,7 @@ template <typename T, T (*f)(T, T), T(*e)()> struct sparse_table {
     int log2_floor(unsigned long long i) {
         return i ? __builtin_clzll(1) - __builtin_clzll(i) : -1;
     }
-    sparse_tree(const vector<T>& a) {
+    sparse_table(const vector<T>& a) {
         n = a.size();
         k = log2_floor(n) + 1;
         st = vector(k, vector<T>(n, e()));
