71c4a86a136f5c61c1c9c744bd48624c9d35f073
Khaybula <khaibula.2003@gmail.com>
2025-12-14 00:37:45 +0300

    fixed typo in extgcd.cpp

--- number_theory/extgcd.cpp
+++ number_theory/extgcd.cpp
@@ -1,4 +1,5 @@
 // x * a + b * y == gcd(a, b)
+// if a b are long long use __int128
 
 ll extgcd(ll a, ll b, ll &x,ll &y)
 {
@@ -6,7 +7,7 @@ ll extgcd(ll a, ll b, ll &x,ll &y)
 		x = 1,y = 0;
 		return a;
 	}
-	ll g=exgcd(b,a%b,x,y),t=x;
+	ll g=extgcd(b,a%b,x,y),t=x;
 	x = y,y = t - a/b * y;
 	return g;
 }
