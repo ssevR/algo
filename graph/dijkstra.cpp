4d2caa1c61b528d96edba2539168c248928725b3
Khaybula <khaibula.2003@gmail.com>
2025-12-20 00:58:30 +0300

    added template for dijkstra

--- graph/dijkstra.cpp
+++ graph/dijkstra.cpp
@@ -1,10 +1,11 @@
-vector<long long> dij(int st, vector<vector<pair<int, int> > >& g) {
+template<typename T>
+vector<T> dij(int st, vector<vector<pair<int, T> > >& g, T inf) {
     int n = g.size();
-    vector<long long> dist(n, LINF); // LINF = 1e18
+    vector<T> dist(n, inf); // LINF = 1e18
 
-    priority_queue<pair<long long, int > , vector<pair<long long, int> >, greater<> > q;
+    priority_queue<pair<T, int > , vector<pair<T, int> >, greater<> > q;
 
-    q.push({0, st});
+    q.push({T(0), st});
     while(!q.empty()) {
         auto [d, v] = q.top();
         q.pop();
