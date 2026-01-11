e18a4d8cf755fedfbe3704470bcace4b6c8b389c
Khaybula <khaibula.2003@gmail.com>
2026-01-11 22:19:00 +0300

    fixed pbds.cpp

--- data_structure/pbds.cpp
+++ data_structure/pbds.cpp
@@ -1,5 +1,11 @@
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
+
 template <typename T>
-using ordered_set =
-    tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>; 
+using ordered_set = __gnu_pbds::tree<
+    T,
+    __gnu_pbds::null_type,
+    less<T>,
+    __gnu_pbds::rb_tree_tag,
+    __gnu_pbds::tree_order_statistics_node_update
+>;
