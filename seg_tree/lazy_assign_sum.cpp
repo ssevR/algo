d6ec05593b851ed656ea80a72e6286f0002f9fa0
Khaybula <khaibula.2003@gmail.com>
2026-02-02 05:54:34 +0300

    added necessary comment for lazy segment tree

--- seg_tree/lazy_assign_sum.cpp
+++ seg_tree/lazy_assign_sum.cpp
@@ -6,4 +6,8 @@ S e() { return {0ll, 0ll}; }
 S mapping(F f, S x){ return (f == ID ? x : make_pair(f * x.second, x.second) ); } 
 F composition(F f, F g){ return (f == ID ? g : f); } 
 F id(){ return ID; } 
-
+/*
+initialize each leaf with {sum = 0, len = 1}
+	vector<S> init(n, make_pair(0ll, 1ll));
+	lazy_segtree<S, op, e, F, mapping, composition, id> seg(init);
+*/
