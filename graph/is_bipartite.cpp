eea67b16eada82ccdc43704d3691b4c5ab5940e1
Khaybula <khaibula.2003@gmail.com>
2026-03-27 21:24:40 +0300

    added is_bipartite.cpp for checking is undirected graph bipartite or no

---
+++ graph/is_bipartite.cpp
@@ -0,0 +1,24 @@
+bool is_bipartite(const vector<vector<int>>&g) {
+	vector<int> colors(g.size(), -1);
+
+	auto dfs = [&](int v, auto&& dfs) -> bool{
+		for (auto u : g[v]) {
+			if (colors[u] == -1) {
+				colors[u] = (colors[v]^1);
+				if (!dfs(u, dfs)) return false;
+			}
+			else if (colors[u] != (colors[v]^1)) {
+				return false;
+			}
+		}
+		return true;
+	};
+
+	for (int i = 0; i < g.size(); ++i)
+		if (colors[i] == -1) {
+			colors[i] = 0;
+			if (!dfs(i, dfs)) return false;
+		}
+
+	return true;
+}
