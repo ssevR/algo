623892ba6994101977c0280c30b227a344950f30
Khaybula <khaibula.2003@gmail.com>
2026-04-04 04:03:16 +0300

    added persistent segtree

---
+++ seg_tree/persistent_segtree.cpp
@@ -0,0 +1,69 @@
+struct persistent_segtree {
+	int n;
+	struct Node {
+		ll sum = 0;
+		shared_ptr<Node> left;
+		shared_ptr<Node> right;
+	};
+	vector<shared_ptr<Node> > roots;
+
+	shared_ptr<Node> build(const vector<int>& v, int l, int r) {
+		shared_ptr<Node> cur(new Node);
+		if (l + 1 == r) {
+			cur.get()->sum = v[l];
+			return cur;
+		}
+		int mid = (r + l) / 2;
+		if (l < mid) {
+			cur.get()->left = shared_ptr<Node>(build(v, l, mid));
+			cur.get()->sum += cur.get()->left.get()->sum;
+		}
+		if (mid < r) {
+			cur.get()->right = shared_ptr<Node>(build(v, mid, r));
+			cur.get()->sum += cur.get()->right.get()->sum;
+		}
+		return cur;
+
+
+	};
+	shared_ptr<Node> update(shared_ptr<Node> cur, int i, int lx, int rx, int x) {
+		if (lx + 1 == rx) {
+			cur.get()->sum = x;
+			return cur;
+		}
+		int mx = (lx + rx) / 2;
+		if (i < mx) {
+			auto old = *(cur.get()->left.get());
+			cur.get()->sum -= old.sum;
+			cur.get()->left = shared_ptr<Node>(new Node(old));
+
+			update(cur.get()->left, i, lx, mx, x);
+			cur.get()->sum += cur.get()->left.get()->sum;
+		}
+		else {
+			auto old = *(cur.get()->right.get());
+			cur.get()->sum -= old.sum;
+			cur.get()->right = shared_ptr<Node>(new Node(old));
+
+			update(cur.get()->right, i, mx, rx, x);
+			cur.get()->sum += cur.get()->right.get()->sum;
+		}
+		return cur;
+	}
+	int update(int ind, int i, int x) {
+		auto new_root = make_shared<Node>(*roots[ind]);
+
+		update(new_root, i, 0, n, x);
+		roots.push_back(new_root);
+		return roots.size() - 1;
+	}
+
+
+	persistent_segtree(const vector<int>& a) {
+		n = a.size();
+
+		int l = 0, r = n;
+		roots.push_back(build(a, 0, n));
+	};
+
+};
